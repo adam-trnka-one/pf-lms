a5764f0b27088891d9f56d7a9fdbad88ba3785ad
adam-trnka-one <trnka@orine.one>
2024-12-16 10:28:18 +0100

    Installed PF

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ProductFruits } from 'react-product-fruits';
 import App from './App.tsx';
 import './index.css';
 
